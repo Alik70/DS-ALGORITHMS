cf280960f4668637eb51de0c8a136b478fb55872
Ali <ak.msm91@gmail.com>
2020-04-25 12:02:01 -0400

    All the files to date

---
+++ main.py
@@ -0,0 +1,54 @@
+from datastructures.StacksDQ import Stack, Queue, Dequeue
+
+s = Stack()
+# print(s.isEmpty())
+# s.push('1')
+# s.push('two')
+# s.push(True)
+# print(s.size())
+# print(s.isEmpty())
+# print(s.pop())
+# print("peek %s" % s.peek())
+
+q = Queue()
+# print(q.isEmpty(), q.size())
+# q.enqueue('first')
+# q.enqueue('second')
+# q.enqueue('third')
+#
+# print(q.dequeue())
+# print(q.dequeue())
+# print(q.dequeue())
+
+d = Dequeue()
+# d.add_front('hello')
+# d.add_rear("world")
+# print(d.items)
+# print(d.size())
+# print(d.remove_front(), d.remove_rear())
+# print(d.size())
+
+
+# a = Node(1)
+# b = Node(2)
+# c = Node(3)
+# a.next_node = b
+# b.next_node = c
+# print(a.next_node.value)
+# print(b.next_node.value)
+
+
+# a = DoublyLinkedList(1)
+# b = DoublyLinkedList(2)
+# c = DoublyLinkedList(3)
+#
+# a.next_node = b
+# b.prev_node = a
+# b.next_node = c
+# c.prev_node = b
+#
+# print(b.prev_node.value, c.prev_node.value, a.next_node.value)
+
+# print(factoriel(99))
+# print(add_fact(10))
+# print(sum_func(12345))
